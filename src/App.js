34cd34b98a512a3f2c7209637c559db9a12b7ef5
Richard Beletatti <richard.beletatti@gmail.com>
2020-12-17 17:37:13 -0300

    commitando

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import "./style.css";
 import Header from './components/Header';
 
 const App = () => (
-<div className="App">
-        <Header />
-        <Routes />
-      </div>
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
 );
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Umbler listening on port %s', port);
+});
 
 export default App; 
